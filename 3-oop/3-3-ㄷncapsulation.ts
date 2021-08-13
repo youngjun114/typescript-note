77d30bb7fd677adabcba3b6440a994475a2858b6
Youngjun Woo <youngjun_92@hotmail.com>
2021-08-13 00:38:14 -0700

    OOP oop example

---
+++ 3-oop/3-3-ㄷncapsulation.ts
@@ -0,0 +1,49 @@
+/**
+ * Encapsulation
+ */
+
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  // public
+  // private
+  // protected
+  class CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT = 7; // cannot bee seen outside the class && class level
+    private coffeeBeans: number = 0; // instance(object) level
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    // if I want to let people use this function instead of creating class instance,
+    // make constructor private
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error('coffee beans cannot be less than 0');
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = CoffeeMaker.makeMachine(32);
+  maker.fillCoffeeBeans(20);
+}
